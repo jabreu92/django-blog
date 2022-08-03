99f32473e5a8ee1b60a63891c4a26058bce5125b
jabreu92 <jose.abreu4@hotmail.com>
2022-08-03 15:14:32 -0700

    init commit

---
+++ mysite/.venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Owner\OneDrive\Desktop\python330\lesson6\starting-django\mysite\.venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
