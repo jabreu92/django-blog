900ba9df055119a5bc801645214cc3177a6838f4
jabreu92 <jose.abreu4@hotmail.com>
2022-08-07 14:28:42 -0700

    About to add static files

--- mysite/blogging/tests.py
+++ mysite/blogging/tests.py
@@ -17,6 +17,18 @@ class CategoryTestCase(TestCase):
         actual = str(c1)
         self.assertEqual(expected, actual)
 
+class PostTestCase(TestCase):
+    fixtures = ['blogging_test_fixture.json', ]
+
+    def setUp(self):
+        self.user = User.objects.get(pk=1)
+
+    def test_string_representation(self):
+        expected = "This is a title"
+        p1 = Post(title=expected)
+        actual = str(p1)
+        self.assertEqual(expected, actual)
+
 class FrontEndTestCase(TestCase):
     """test views provided in the front-end"""
     fixtures = ['blogging_test_fixture.json', ]
@@ -46,3 +58,14 @@ class FrontEndTestCase(TestCase):
                 self.assertContains(resp, title, count=1)
             else:
                 self.assertNotContains(resp, title)
+    
+    def test_details_only_published(self):
+        for count in range(1, 11):
+            title = "Post %d Title" % count
+            post = Post.objects.get(title=title)
+            resp = self.client.get('/posts/%d/' % post.pk)
+            if count < 6:
+                self.assertEqual(resp.status_code, 200)
+                self.assertContains(resp, title)
+            else:
+                self.assertEqual(resp.status_code, 404)
