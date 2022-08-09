731dccbea5e75c5e26b2779d9787a51a831f286f
jabreu92 <jose.abreu4@hotmail.com>
2022-08-09 10:46:25 -0700

    class view working

--- mysite/.venv/Scripts/activate.bat
+++ mysite/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Owner\OneDrive\Desktop\python330\lesson6\starting-django\mysite\.venv
+set VIRTUAL_ENV=C:\Users\Owner\OneDrive\Desktop\python330\lesson8\django-blog\mysite\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- mysite/blogging/views.py
+++ mysite/blogging/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import RequestContext, loader
 from blogging.models import Post
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 def stub_view(request, *args, **kwargs):
     body = "Stub View\n\n"
@@ -12,7 +14,8 @@ def stub_view(request, *args, **kwargs):
         body += "Kwargs:\n"
         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
     return HttpResponse(body, content_type="text/plain")
-# and this view
+
+
 def list_view(request):
     published = Post.objects.exclude(published_date__exact=None)
     posts = published.order_by('-published_date')
@@ -20,6 +23,16 @@ def list_view(request):
     context = {'posts': posts}
     body = template.render(context)
     return HttpResponse(body, content_type="text/html")
+
+class BloggingListView(ListView):
+    model = Post
+    template_name = 'blogging/list.html'
+
+class BloggingDetailView(DetailView):
+    model = Post
+    template_name = 'blogging/detail.html'
+
+
 
 def detail_view(request, post_id):
     published = Post.objects.exclude(published_date__exact=None)
