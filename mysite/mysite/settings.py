b9bd5c086405b21e64c6d71c5b32b81cbc6b6310
jabreu92 <jose.abreu4@hotmail.com>
2022-08-16 12:02:29 -0700

    had to pip install allauth

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django.contrib.sites', # new
+    'allauth', # new
+    'allauth.account', # new
+    'allauth.socialaccount', # new
+    'allauth.socialaccount.providers.github', # new
     "polling",
     "blogging",
 ]
@@ -123,4 +128,14 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 LOGIN_URL = "/login/"
-LOGIN_REDIRECT_URL = "/"
+# config/settings.py
+AUTHENTICATION_BACKENDS = (
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+)
+
+SITE_ID = 1
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+LOGIN_REDIRECT_URL = '/'
